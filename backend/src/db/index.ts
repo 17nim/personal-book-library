cf0e58a223fef55663d9c7463a4f0f4ed75895f7
17nim <17nim.m@gmail.com>
2026-09-15 23:14:25 +0700

    feat: export db instance for external usage

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
@@ -5,4 +6,4 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL!,
 });
 
-const db = drizzle({ client: pool });
+export const db = drizzle({ client: pool });
